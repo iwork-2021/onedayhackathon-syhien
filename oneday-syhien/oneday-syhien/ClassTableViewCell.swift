cdc82b4d77f265434ed760e8f22fb0bb7816ef87
syhien <syhien@outlook.at>
2021-12-21 14:44:58 +0800

    实现了识别分类的table view的ui

---
+++ oneday-syhien/oneday-syhien/ClassTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ClassTableViewCell.swift
+//  oneday-syhien
+//
+//  Created by nju on 2021/12/21.
+//
+
+import UIKit
+
+class ClassTableViewCell: UITableViewCell {
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ oneday-syhien/oneday-syhien/DetectedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetectedViewController.swift
+//  oneday-syhien
+//
+//  Created by nju on 2021/12/21.
+//
+
+import UIKit
+
+class DetectedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
