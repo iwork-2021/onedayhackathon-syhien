26f99ce8243154f5cbc89eb57a3f131e0ecd1ac4
syhien <syhien@outlook.at>
2021-12-21 12:52:37 +0800

    尝试创建view用于添加照片

---
+++ oneday-syhien/oneday-syhien/AddPhotoViewController.swift
@@ -0,0 +1,46 @@
+//
+//  AddPhotoViewController.swift
+//  oneday-syhien
+//
+//  Created by nju on 2021/12/21.
+//
+
+import UIKit
+
+class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    
+    let photoPicker = UIImagePickerController()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        photoPicker.delegate = self
+        photoPicker.sourceType = .photoLibrary
+    }
+    
+
+    @IBAction func addPhoto(_ sender: UIButton) {
+        present(photoPicker, animated: true)
+    }
+    
+    // MARK: - UIImagePickerControllerDelegate Methods
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            //add
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
