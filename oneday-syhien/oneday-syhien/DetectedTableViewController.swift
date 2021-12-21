b82756363311acb13fa7e2d45a3694f14542acd3
syhien <syhien@outlook.at>
2021-12-21 15:09:49 +0800

    实现了查看单类

--- oneday-syhien/oneday-syhien/DetectedTableViewController.swift
+++ oneday-syhien/oneday-syhien/DetectedTableViewController.swift
@@ -98,14 +98,15 @@ class DetectedTableViewController: UITableViewController {
     }
     */
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        let singleCollectionViewController = segue.destination as! SingleCollectionViewController
+        let cell = sender as! ClassTableViewCell
+        singleCollectionViewController.className = cell.label.text!
     }
-    */
 
 }
---
+++ oneday-syhien/oneday-syhien/SingleCellCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SingleCellCollectionViewCell.swift
+//  oneday-syhien
+//
+//  Created by nju on 2021/12/21.
+//
+
+import UIKit
+
+class SingleCellCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
