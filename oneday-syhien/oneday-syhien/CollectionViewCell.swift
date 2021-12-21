141ea3bf9a239289c3bac36ea7045a74b5dd664b
syhien <syhien@outlook.at>
2021-12-21 11:48:37 +0800

    实现了浏览所有图片的界面

---
+++ oneday-syhien/oneday-syhien/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  oneday-syhien
+//
+//  Created by nju on 2021/12/21.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
---
+++ oneday-syhien/oneday-syhien/ViewAllFlowLayout.swift
@@ -0,0 +1,12 @@
+//
+//  ViewAllFlowLayout.swift
+//  oneday-syhien
+//
+//  Created by nju on 2021/12/21.
+//
+
+import UIKit
+
+class ViewAllFlowLayout: UICollectionViewFlowLayout {
+    
+}
